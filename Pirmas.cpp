2404b2eef453babadd0edecb676fe7891d6c17c6
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-02-07 22:25:04 +0200

    Add files via upload

--- Pirmas.cpp
+++ Pirmas.cpp
@@ -2,12 +2,14 @@
 #include <cmath>
 #include <math.h>
 #include <stdlib.h>
+#include <iomanip>
 
 using namespace std;
 
 int s, nd;
+double vidurkis, galutinis;
 
-struct duomenys {
+struct Duomenys {
     string vardas;
     string pavarde;
     int tarpiniai_rezultatai;
@@ -16,28 +18,28 @@ struct duomenys {
 
 int main() {
 
-    duomenys d;
+    cout << "Kiek yra studentu? "; cin >> s;
+    cout << "Kiek namu darbu buvo uzduota? "; cin >> nd;
 
-    cout << "Kiek yra studentu? ";
-    cin >> s;
-    cout << "Kiek namu darbu buvo uzduota? ";
-    cin >> nd;
+    Duomenys *studentas = new Duomenys[s];
 
-    for (int i = 1; i <= s; i++) {
+    for (int i = 0; i < s; i++) {
 
-        cout << "Iveskite " << i << " studento varda: ";
-        cin >> d.vardas;
-        cout << "Iveskite " << i << " studento pavarde: ";
-        cin >> d.pavarde;
+        cout << "Iveskite " << i+1 << " studento varda: "; cin >> studentas[i].vardas;
+        cout << "Iveskite " << i+1 << " studento pavarde: "; cin >> studentas[i].pavarde;
 
-        for (int j = 1; j <= nd; j++) {
+        for (int j = 0; j < nd; j++) {
 
-            cout << "Iveskite studento pazymi uz " << j << " namu darba: ";
-            cin >> d.tarpiniai_rezultatai;
+            cout << "Iveskite studento pazymi uz " << j+1 << " namu darba: "; cin >> studentas[j].tarpiniai_rezultatai;
         }
 
-        cout << "Iveskite " << i << " studento egzamino rezultata: ";
-        cin >> d.egzamino_rezultatas;
+        cout << "Iveskite " << i+1 << " studento egzamino rezultata: "; cin >> studentas[i].egzamino_rezultatas;
+
+        vidurkis = 1.00 * studentas[i].tarpiniai_rezultatai / nd;
+        galutinis = 0.4 * vidurkis + 0.6 * studentas[i].egzamino_rezultatas;
+        cout << "Vidurkis = " << vidurkis << endl; //Blogai skaiciuoja vidurki, del to kencia galutinis rezultatas
+        cout << "Galutinis (Vid.) = " << fixed << setprecision(2) << galutinis << endl;
+        //Padaryti taip, kad pazymis <= 10, bet >0 arba >=1
 
         cout << "---------------------------------------" << endl;
     }
