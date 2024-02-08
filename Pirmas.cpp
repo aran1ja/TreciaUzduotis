444a796c4b6357e3f81f544c84190deffeaa9c6a
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-02-08 23:20:59 +0200

    Add files via upload

--- Pirmas.cpp
+++ Pirmas.cpp
@@ -3,25 +3,31 @@
 #include <math.h>
 #include <stdlib.h>
 #include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
 int s, nd, pasirinkimas;
-double suma, vidurkis, mediana;
+double suma, vidurkis;
 
 struct Studentas {
-    string vardas;
-    string pavarde;
-    double galutinis_vid;
-    int pazymiai;
-    int egzamino_rezultatas;
+    string vardas, pavarde;
+    double galutinis_vid, mediana;
+    int pazymiai, egzamino_rezultatas;
 };
 
 int main() {
 
     cout << "Kiek yra studentu? "; cin >> s;
-    cout << "Kiek namu darbu buvo uzduota? "; cin >> nd;
+    while (s <= 0) {
+        cout << "Klaida. Bandykite ivesti kita kieki. "; cin >> s;
+    }
 
+    cout << "Kiek namu darbu buvo uzduota? "; cin >> nd;
+    while (nd < 0) {
+        cout << "Klaida. Bandykite ivesti kita kieki. "; cin >> nd;
+    }
+        
     Studentas *studentas = new Studentas[s];
     int **pazymiai = new int*[s];
 
@@ -42,9 +48,7 @@ int main() {
                 cout << "Ivyko klaida. Prasau irasykite skaiciu nuo 1 iki 10." << endl;
                 cout << "Studento pazymis uz " << j+1 << " namu darba: "; cin >> pazymiai[i][j];
             } 
-
                suma += pazymiai[i][j]; //Skaiciuoja suma visu pazymiu  
-               
                }
             
             cout << "Studento egzamino rezultatas: "; cin >> studentas[i].egzamino_rezultatas;
@@ -54,10 +58,10 @@ int main() {
                 cout << "Studento egzamino rezultatas: "; cin >> studentas[i].egzamino_rezultatas;
                 }
 
-            //Tu ma byc mediana
-            //Porownac w pazymis i i j, oraz egzamino rezult.
-            //if ( i )
-            //mediana = 0.5 * (pazymiai[i][(nd - 1) / 2] + pazymiai[i][nd / 2]); //Ne visada gerai skaiciuoja rezultata
+        //Mediana
+        studentas[i].mediana = 0.5 * (pazymiai[i][(nd - 1) / 2] + pazymiai[i][nd / 2]);
+        //Liczy mediane tylko z ocen, NIE z egzaminu wlacznie
+        //Liczy tylko podany srodek - NIE MA sortowania cyfr od najmniejszej do najwiekszej
 
         //Jeigu nebuvo jokiu namu darbu, galutinis vidurkis skaiciuojamas tik is egzamino rezultato
         if (nd == 0) {
@@ -69,8 +73,6 @@ int main() {
         //Galutinio vidurkio skaiciavimas
         studentas[i].galutinis_vid = 0.4 * vidurkis + 0.6 * studentas[i].egzamino_rezultatas;
         cout << "Vidurkis = " << fixed << setprecision(2) << vidurkis << endl; 
-        //cout << "Galutinis (Med.) = " << fixed << setprecision(2) << mediana << endl; 
-
         cout << "--------------------------------------------------" << endl;
     }
 
@@ -79,7 +81,9 @@ int main() {
         cout << "Jeigu reikalingas vidurkis, spauskite 1.\nJeigu reikalinga mediana, spauskite 2." << endl;
         cin >> pasirinkimas;
 
+
         while (pasirinkimas != 1 || pasirinkimas != 2) {
+
             cout << "Klaida. Bandykite ivesti reikiama skaiciu dar karta." << endl;
             cout << "Spauskite 1 arba 2. "; cin >> pasirinkimas;
 
@@ -92,17 +96,16 @@ int main() {
                  cout << studentas[i].vardas << "\t " << studentas[i].pavarde << "\t ";
                 cout << fixed << setprecision(2) << studentas[i].galutinis_vid << endl;
             } break;
+
         } else if (pasirinkimas == 2) {
 
             cout << "Vardas\t Pavarde\t Galutinis (Med.)" << endl;
             cout << "--------------------------------------------------" << endl;
-            
+
             for (int i = 0; i < s; i++) {
                  cout << studentas[i].vardas << "\t " << studentas[i].pavarde << "\t ";
                 cout << "Cia turi buti mediana" << endl;
             } break;
         }
-        }
-
-        
+    }
 }
