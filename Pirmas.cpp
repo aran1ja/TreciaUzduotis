ecf9297f3be440746e36014da658cf50123ef9a5
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-02-08 11:06:31 +0200

    Add files via upload

--- Pirmas.cpp
+++ Pirmas.cpp
@@ -12,7 +12,7 @@ double suma, vidurkis, galutinis_vid, mediana;
 struct Duomenys {
     string vardas;
     string pavarde;
-    int tarpiniai_rezultatai;
+    int pazymiai;
     int egzamino_rezultatas;
 };
 
@@ -22,6 +22,7 @@ int main() {
     cout << "Kiek namu darbu buvo uzduota? "; cin >> nd;
 
     Duomenys *studentas = new Duomenys[s];
+    int **pazymiai = new int*[s];
 
     for (int i = 0; i < s; i++) {
 
@@ -30,17 +31,21 @@ int main() {
         cout << "Iveskite " << i+1 << " studento varda: "; cin >> studentas[i].vardas;
         cout << "Iveskite " << i+1 << " studento pavarde: "; cin >> studentas[i].pavarde;
 
+        pazymiai[i] = new int[nd];
+
         for (int j = 0; j < nd; j++) {
 
-            cout << "Studento pazymis uz " << j+1 << " namu darba: "; cin >> studentas[j].tarpiniai_rezultatai;
+            cout << "Studento pazymis uz " << j+1 << " namu darba: "; cin >> pazymiai[i][j];
 
-            while (studentas[j].tarpiniai_rezultatai > 10 || studentas[j].tarpiniai_rezultatai < 1) {
+            while (pazymiai[i][j] > 10 || pazymiai[i][j] < 1) {
                 cout << "Ivyko klaida. Prasau irasykite skaiciu nuo 1 iki 10." << endl;
-                cout << "Studento pazymis uz " << j+1 << " namu darba: "; cin >> studentas[j].tarpiniai_rezultatai;
+                cout << "Studento pazymis uz " << j+1 << " namu darba: "; cin >> pazymiai[i][j];
             } 
-               suma += studentas[j].tarpiniai_rezultatai;
+               suma += pazymiai[i][j]; //Skaiciuoja suma visu pazymiu  
+               mediana = 0.5 * (pazymiai[i][(nd - 1) / 2] + pazymiai[i][nd / 2]); //Ne visada gerai skaiciuoja rezultata
                }
-
+            
+            
             cout << "Studento egzamino rezultatas: "; cin >> studentas[i].egzamino_rezultatas;
 
             while (studentas[i].egzamino_rezultatas > 10 || studentas[i].egzamino_rezultatas < 1) {
@@ -50,8 +55,9 @@ int main() {
 
         vidurkis = suma / nd * 1.00;
         galutinis_vid = 0.4 * vidurkis + 0.6 * studentas[i].egzamino_rezultatas;
-        cout << "Vidurkis = " << vidurkis << endl; 
+        cout << "Vidurkis = " << fixed << setprecision(2) << vidurkis << endl; 
         cout << "Galutinis (Vid.) = " << fixed << setprecision(2) << galutinis_vid << endl;
+        cout << "Galutinis (Med.) = " << fixed << setprecision(2) << mediana << endl; 
 
         cout << "---------------------------------------" << endl;
     }
