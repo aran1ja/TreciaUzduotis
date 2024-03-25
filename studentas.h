b7f2096b859d7158ff0060ff8cd1e1cad687901e
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-03-25 18:26:11 +0200

    Add files via upload

--- studentas.h
+++ studentas.h
@@ -6,11 +6,19 @@
 
 using namespace std;
 
-struct Studentas {
+class Studentas {
+
+    public:
     vector<int> pazymiai;
     string vardas, pavarde;
     int egzamino_rezultatas;
     double galutinis_vid, mediana;
+
+    Studentas() : egzamino_rezultatas(0), galutinis_vid(0), mediana(0) {}
+    Studentas(const vector<int>& pazymiai, string vardas, string pavarde, int egzamino_rezultatas, double galutinis_vid, double mediana) 
+        : pazymiai(pazymiai), vardas(vardas), pavarde(pavarde), egzamino_rezultatas(egzamino_rezultatas), galutinis_vid(galutinis_vid), mediana(mediana) {}
+
+    ~Studentas() {}
 };
 
 #endif
