097e2188e601832da126e8d705a5887e10905dce
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-03-25 14:35:42 +0200

    Add files via upload

--- studentas.h
+++ studentas.h
@@ -14,6 +14,20 @@ class Studentas {
         int egzamino_rezultatas_;
         double galutinis_vid_;
         double mediana_;
+
+    public:
+        // Konstruktorius
+        Studentas() : egzamino_rezultatas_(0), galutinis_vid_(0), mediana_(0) {}
+
+    // Metodai
+    inline string vardas() const { return vardas_; }
+    inline string pavarde() const { return pavarde_; }
+    double galBalas(double (*funkcija)(vector<int>) = mediana) const;
+    istream& readStudent(istream&);
 };
+
+bool compare(const Studentas&, const Studentas&);
+bool comparePagalPavarde(const Studentas&, const Studentas&);
+bool comparePagalEgza(const Studentas&, const Studentas&);
 
 #endif
