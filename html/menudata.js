87431b8da4b937114c70896bfe6d3f233a8ec854
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-05-06 18:22:09 +0300

    Add files via upload

--- html/menudata.js
+++ html/menudata.js
@@ -31,9 +31,7 @@ var menudata={children:[
 {text:"Class Members",url:"functions.html",children:[
 {text:"All",url:"functions.html",children:[
 {text:"a",url:"functions.html#index_a"},
-{text:"e",url:"functions.html#index_e"},
 {text:"g",url:"functions.html#index_g"},
-{text:"m",url:"functions.html#index_m"},
 {text:"o",url:"functions.html#index_o"},
 {text:"p",url:"functions.html#index_p"},
 {text:"s",url:"functions.html#index_s"},
