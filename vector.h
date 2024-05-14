ff528fd799684027ad548a1920f03bd58136a930
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-05-14 13:25:48 +0300

    Add files via upload

--- vector.h
+++ vector.h
@@ -10,10 +10,10 @@ template<typename T>
 class Vector {
 private:
     T* data;
-    std::size_t size;
-    std::size_t capacity;
+    size_t size;
+    size_t capacity;
 
-    void resize(std::size_t new_capacity) {
+    void resize(size_t new_capacity) {
         T* new_data = new T[new_capacity];
         std::copy(data, data + size, new_data);
         delete[] data;
@@ -22,9 +22,10 @@ private:
     }
 
 public:
+
     // Member types
     using value_type = T;
-    using allocator_type = Allocator;
+    using allocator_type = std::allocator<T>;
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
     using reference = T&;
@@ -36,6 +37,74 @@ public:
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+    // Member functions
+    Vector() : data(nullptr), size(0), capacity(0) {}
+    ~Vector() {
+        delete[] data;
+    }
+    Vector& operator=(const Vector& other) {
+        if (this != &other) {
+            delete[] data;
+            size = other.size;
+            capacity = other.capacity;
+            data = new T[capacity];
+            std::copy(other.data, other.data + size, data);
+        }
+        return *this;
+    }
+
+    // Element access
+    reference at(size_type index) {
+        if (index >= size) {
+            throw std::out_of_range("Vector::at index out of range");
+        }
+        return data[index];
+    }
+    const_reference at(size_type index) const {
+        if (index >= size) {
+            throw std::out_of_range("Vector::at index out of range");
+        }
+        return data[index];
+    }
+    reference operator[](size_type index) { return data[index]; }
+    const_reference operator[](size_type index) const { return data[index]; }
+    reference front() { return data[0]; }
+    const_reference front() const { return data[0]; }
+    reference back() { return data[size - 1]; }
+    const_reference back() const { return data[size - 1]; }
+    pointer data() noexcept { return data; }
+    const_pointer data() const noexcept { return data; }
+
+    // Iterators
+    iterator begin() noexcept { return data; }
+    const_iterator begin() const noexcept { return data; }
+    iterator end() noexcept { return data + size; }
+    const_iterator end() const noexcept { return data + size; }
+    reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+    const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
+    reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+    const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
+
+    // Capacity
+    bool empty() const noexcept { return size == 0; }
+    size_type size() const noexcept { return size; }
+    size_type max_size() const noexcept { return std::numeric_limits<size_type>::max(); }
+    void reserve(size_type new_capacity) {
+        if (new_capacity > capacity) {
+            resize(new_capacity);
+        }
+    }
+    size_type capacity() const noexcept { return capacity; }
+    void shrink_to_fit() {
+        if (size < capacity) {
+            resize(size);
+        }
+    }
+
+    // Modifiers
+    
+    
+
 };
 
-#endif // VECTOR_H
+#endif
