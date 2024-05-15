5f632d9efd2a537a15cfe40cc58a4db58656af05
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-05-15 10:43:44 +0300

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -59,3 +59,56 @@ TEST(VectorTest, BackFunkcija) {
     Vector<int> v = { 1, 2, 3, 4, 5 };
     EXPECT_EQ(v.back(), 5);
 }
+
+TEST(VectorTest, Size) {
+    Vector<int> v;
+    EXPECT_EQ(v.size(), 0);
+    v.push_back(10);
+    EXPECT_EQ(v.size(), 1);
+}
+
+TEST(VectorTest, Resize) {
+    Vector<int> v;
+    v.resize(5);
+    EXPECT_EQ(v.size(), 5);
+    v.resize(3);
+    EXPECT_EQ(v.size(), 3);
+}
+
+TEST(VectorTest, Capacity) {
+    Vector<int> v;
+    v.resize(10);
+    EXPECT_GE(v.capacity(), v.size());
+}
+
+TEST(VectorTest, Empty) {
+    Vector<int> v;
+    EXPECT_TRUE(v.empty());
+    v.push_back(5);
+    EXPECT_FALSE(v.empty());
+}
+
+TEST(VectorTest, Reserve) {
+    Vector<int> v;
+    v.reserve(100);
+    EXPECT_GE(v.capacity(), 100);
+}
+
+TEST(VectorTest, Clear) {
+    Vector<int> v;
+    v.push_back(5);
+    v.clear();
+    EXPECT_TRUE(v.empty());
+}
+
+TEST(VectorTest, LygusVektoriai) {
+    Vector<int> v1{ 1, 2, 3 };
+    Vector<int> v2{ 1, 2, 3 };
+    EXPECT_TRUE(v1 == v2);
+}
+
+TEST(VectorTest, NelygusVektoriai) {
+    Vector<int> v1{ 1, 2, 3 };
+    Vector<int> v2{ 1, 2, 4 };
+    EXPECT_TRUE(v1 != v2);
+}
